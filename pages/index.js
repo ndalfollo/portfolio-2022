76c2ead4f2d5a8a12829115104139fa18603000f
Nathan <n.dalf@protonmail.com>
2023-01-05 11:06:05 +0100

    cursor problem solved on prod build ?

--- pages/index.js
+++ pages/index.js
@@ -6,54 +6,53 @@ import Cursor from './Components/Cursor'
 import { useEffect } from 'react'
 
 export default function Home() {
+  if (typeof window !== 'undefined') {
+    const lerp = (a, b, n) => (1 - n) * a + n * b;
 
-  useEffect(() => {
-  const lerp = (a, b, n) => (1 - n) * a + n * b;
-
-class Cursor {
-  constructor() {
-    // config
-    this.target = { x: 0.5, y: 0.5 }; // mouse position
-    this.cursor = { x: 0.5, y: 0.5 }; // cursor position
-    this.speed = 0.2;
-    this.init();
-  }
-  bindAll() {
-    ["onMouseMove", "render"].forEach((fn) => (this[fn] = this[fn].bind(this)));
-  }
-  onMouseMove(e) {
-    //get normalized mouse coordinates [0, 1]
-    this.target.x = e.clientX / window.innerWidth;
-    this.target.y = e.clientY / window.innerHeight;
-    // trigger loop if no loop is active
-    if (!this.raf) this.raf = requestAnimationFrame(this.render);
-  }
-  render() {
-    //calculate lerped values
-    this.cursor.x = lerp(this.cursor.x, this.target.x, this.speed);
-    this.cursor.y = lerp(this.cursor.y, this.target.y, this.speed);
-    document.documentElement.style.setProperty("--cursor-x", this.cursor.x);
-    document.documentElement.style.setProperty("--cursor-y", this.cursor.y);
-    //cancel loop if mouse stops moving
-    const delta = Math.sqrt(
-      Math.pow(this.target.x - this.cursor.x, 2) +
-      Math.pow(this.target.y - this.cursor.y, 2)
-    );
-    if (delta < 0.001) {
-      cancelAnimationFrame(this.raf);
-      this.raf = null;
-      return;
+    class Cursor {
+      constructor() {
+        // config
+        this.target = { x: 0.5, y: 0.5 }; // mouse position
+        this.cursor = { x: 0.5, y: 0.5 }; // cursor position
+        this.speed = 0.2;
+        this.init();
+      }
+      bindAll() {
+        ["onMouseMove", "render"].forEach((fn) => (this[fn] = this[fn].bind(this)));
+      }
+      onMouseMove(e) {
+        //get normalized mouse coordinates [0, 1]
+        this.target.x = e.clientX / window.innerWidth;
+        this.target.y = e.clientY / window.innerHeight;
+        // trigger loop if no loop is active
+        if (!this.raf) this.raf = requestAnimationFrame(this.render);
+      }
+      render() {
+        //calculate lerped values
+        this.cursor.x = lerp(this.cursor.x, this.target.x, this.speed);
+        this.cursor.y = lerp(this.cursor.y, this.target.y, this.speed);
+        document.documentElement.style.setProperty("--cursor-x", this.cursor.x);
+        document.documentElement.style.setProperty("--cursor-y", this.cursor.y);
+        //cancel loop if mouse stops moving
+        const delta = Math.sqrt(
+          Math.pow(this.target.x - this.cursor.x, 2) +
+          Math.pow(this.target.y - this.cursor.y, 2)
+        );
+        if (delta < 0.001) {
+          cancelAnimationFrame(this.raf);
+          this.raf = null;
+          return;
+        }
+        //or continue looping if mouse is moving
+        this.raf = requestAnimationFrame(this.render);
+      }
+      init() {
+          this.bindAll();
+          window.addEventListener("mousemove", this.onMouseMove);
+          this.raf = requestAnimationFrame(this.render);
+      }
     }
-    //or continue looping if mouse is moving
-    this.raf = requestAnimationFrame(this.render);
-  }
-  init() {
-    this.bindAll();
-    window.addEventListener("mousemove", this.onMouseMove);
-    this.raf = requestAnimationFrame(this.render);
-  }
 }
-})
 
 new Cursor();
 
@@ -61,7 +60,7 @@ new Cursor();
     <div className={styles.container}>
       <Head />
       <main className={styles.main}>
-        <div class="cursor"></div>
+        <div className={"cursor"}></div>
         <Nav />
         <div className={styles.projects}>
           <div class="project-grid">
